9db71fc553df24a8f7794729716e1dfd17498f40
Rudra356 <rudranaskar66@gmail.com>
2025-04-19 23:40:37 +0530

    Commit of Day 2

--- Day_2/p1.java
+++ Day_2/p1.java
@@ -7,13 +7,19 @@ import java.util.List;
 public class p1 {
 
     public static void main(String[] args) {
-    String  str = "India is a nunu";
+        String  str = "India is ann ni";
         ArrayList<String> al  = new ArrayList<>(List.of(str.split(" ")));
         long a = al.stream().filter(x ->
                 x.toLowerCase().startsWith("a")||x.toLowerCase().startsWith("e")||
                         x.toLowerCase().startsWith("i")||x.toLowerCase().toLowerCase().startsWith("o")||
                         x.toLowerCase().startsWith("u")).count();
         System.out.println(a);
+
+        long b = al.stream().filter(x->{
+            String wp = x.toLowerCase();
+            return !(wp.endsWith("a")||wp.endsWith("e")||wp.endsWith("i")||wp.endsWith("o")||wp.endsWith("u"));
+        }).count();
+        System.out.println(b);
     }
 
 }
