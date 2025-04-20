b3f9ffd2f1648ca211fab7528288077a71c60f09
Rudra356 <rudranaskar66@gmail.com>
2025-04-20 11:39:42 +0530

    Commit of Day 3

--- Day_3/p5.java
+++ Day_3/p5.java
@@ -1,7 +1,8 @@
 package Day_3;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
-
+import java.util.List;
 
 public class p5 {
     public static void main(String[] args) {
@@ -12,5 +13,13 @@ public class p5 {
         que.removeFirst();
         que.removeFirst();
         System.out.println(que.getLast());
+
+        ArrayList<Integer> al = new ArrayList<>(List.of(1,3,5));
+        ArrayList<Integer> al2 = new ArrayList<>(List.of(2,4,6));
+        ArrayList<Integer> al3 = new ArrayList<>();
+        al3.addAll(al);
+        al3.addAll(al2);
+        al3.sort((a,b)->a-b);
+        System.out.println(al3);
     }
 }
